26a3a4ee717539bb2d205add99bd389df7e2eced
YoungKrug <31873199+YoungKrug@users.noreply.github.com>
2023-09-30 08:09:05 -0400

    Camera movement, and rotation have been separated out and following the srp principle.

--- Assets/_Scripts/Movement/CameraMovement.cs
+++ Assets/_Scripts/Movement/CameraMovement.cs
@@ -1,47 +1,47 @@
-﻿using System;
-using _Scripts.Interface;
+﻿using _Scripts.Interface;
 using _Scripts.ScriptableObjects;
 using UnityEngine;
 
 namespace _Scripts.Movement
 {
-    
-    //TODO Create a camera rotation system
-    public class CameraMovement : MonoBehaviour, IInputReceivers
+    public class CameraMovement : IInputReceivers
     {
         public KeyCode[] Keys { get; } = new KeyCode[] {KeyCode.A, KeyCode.D, KeyCode.S, KeyCode.W};
-        public Transform cameraObject;
-        private readonly float speed = 5f;
-        public BaseEventScriptableObject inputScriptableObject;
-        private void MoveCamera(KeyCode keyCode)
+        private readonly Transform _camera;
+        private const float Speed = 5f;
+
+        public CameraMovement(BaseEventScriptableObject inputEventScriptableObject, Transform camera)
+        {
+            inputEventScriptableObject.OnEventRaised(this);
+            _camera = camera;
+        }
+
+        private void MoveCamera(KeyCode keyCode, Transform cameraObject)
         {
             switch (keyCode)
             {
                 case KeyCode.A:
                     cameraObject.Translate(new 
-                        Vector3(-speed * Time.deltaTime, 0));
+                        Vector3(-Speed * Time.deltaTime, 0));
                     break;
                 case KeyCode.W:
                     cameraObject.Translate(new 
-                        Vector3(0, 0, speed * Time.deltaTime));
+                        Vector3(0, 0, Speed * Time.deltaTime));
                     break;
                 case KeyCode.D:
                     cameraObject.Translate(new 
-                        Vector3(speed * Time.deltaTime, 0));
+                        Vector3(Speed * Time.deltaTime, 0));
                     break;
                 case KeyCode.S:
                     cameraObject.Translate(new 
-                        Vector3(0, 0, -speed * Time.deltaTime));
+                        Vector3(0, 0, -Speed * Time.deltaTime));
                     break;
             }
         }
-        void Start()
+        
+        public void ExecuteKey(KeyCode code)
         {
-            inputScriptableObject.OnEventRaised(this);
-        }
-        public void ExecuteKey(KeyCode key)
-        {
-            MoveCamera(key);
+            MoveCamera(code, _camera);
         }
     }
 }
--- Assets/_Scripts/Movement/CameraRotation.cs
+++ Assets/_Scripts/Movement/CameraRotation.cs
@@ -1,22 +1,32 @@
-﻿using UnityEngine;
+﻿using _Scripts.Interface;
+using _Scripts.ScriptableObjects;
+using UnityEngine;
 
 namespace _Scripts.Movement
 {
-    public class CameraRotation : MonoBehaviour
+    public class CameraRotation : IInputReceivers
     {
-        
-        public float speedH = 2.0f;
-        public float speedV = 2.0f;
-
-        private float yaw = 0.0f;
-        private float pitch = 0.0f;
-
-        void Update()
+        public KeyCode[] Keys { get; } = new KeyCode[] { KeyCode.Mouse0 };
+        private const float SpeedH = 2.0f;
+        private const float SpeedV = 2.0f;
+        private readonly Transform _camera;
+        private float _yaw = 0.0f;
+        private float _pitch = 0.0f;
+        public CameraRotation(BaseEventScriptableObject inputScriptableObject, Transform
+             camera)
         {
-            if (!Input.GetMouseButton(0)) return;
-            yaw += speedH * Input.GetAxis("Mouse X");
-            pitch -= speedV * Input.GetAxis("Mouse Y");
-            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+            inputScriptableObject.OnEventRaised(this);
+            _camera = camera;
+        }
+        private void Rotate() // Rotates the camera
+        {
+            _yaw += SpeedH * Input.GetAxis("Mouse X");
+            _pitch -= SpeedV * Input.GetAxis("Mouse Y");
+            _camera.eulerAngles = new Vector3(_pitch, _yaw, 0.0f);
+        }
+        public void ExecuteKey(KeyCode code)
+        {
+            Rotate();
         }
     }
 }
